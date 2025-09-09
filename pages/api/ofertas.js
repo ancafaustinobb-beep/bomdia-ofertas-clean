fccd53ed7f8d0a448ce2513d6b43ba1f2efc2f60
Antonio Carlos Faustino <ancafaustinobb@gmail.com>
2025-09-08 22:22:52 -0300

    Add files via upload

--- pages/api/ofertas.js
+++ pages/api/ofertas.js
@@ -1,3 +1,5 @@
+// pages/api/ofertas.js
+
 export default function handler(req, res) {
   const ofertas = [
     {
@@ -15,14 +17,25 @@ export default function handler(req, res) {
       "discount": "12%"
     },
     {
-      {
-  "title": "Notebook VAIO FE15, Intel Core i7 12th, 16GB 512GB SSD, Tela 15,6 Full HD Antirreflexo, Linux - Cinza Grafite",
-  "image": "https://m.media-amazon.com/images/I/71wnc1MahXL._AC_SX522_.jpg",
-  "link": "https://amzn.to/46suSSe",
-  "price": "R$ 5.499,00",
-  "discount": "15%"
-}
-
+      "title": "Notebook VAIO FE15, Intel Core i7 12th, 16GB 512GB SSD, Tela 15,6 Full HD Antirreflexo, Linux - Cinza Grafite",
+      "image": "https://m.media-amazon.com/images/I/71wnc1MahXL._AC_SX522_.jpg",
+      "link": "https://amzn.to/46suSSe",
+      "price": "R$ 5.499,00",
+      "discount": "15%"
+    },
+    {
+      "title": "Fone de Ouvido Bluetooth Sony WH-1000XM5",
+      "image": "https://m.media-amazon.com/images/I/61xXU1w5+XL._AC_SX679_.jpg",
+      "link": "https://amzn.to/46abcde",
+      "price": "R$ 1.799,00",
+      "discount": "8%"
+    },
+    {
+      "title": "Smart TV Samsung 55\" QLED 4K",
+      "image": "https://m.media-amazon.com/images/I/81Tv+Q3s+XL._AC_SX679_.jpg",
+      "link": "https://amzn.to/46fghij",
+      "price": "R$ 4.199,00",
+      "discount": "10%"
     }
   ];
 
